ccf8d5b1babd6d2faca57ffaaa21f23765451774
Victor Massé <victor.dhamma@gmail.com>
2024-03-13 10:26:02 +0100

    up

---
+++ src/assets/main.js
@@ -0,0 +1,3 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("app", () => ({ count: 0 }))
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  darkMode: "class",
+  important: "#tailwindSelector",
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/container-queries"),
+  ],
+}

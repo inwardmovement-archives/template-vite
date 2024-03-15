500172f4880e0e556baf8cdee87a86cec35f6ac8
Victor Massé <victor.dhamma@gmail.com>
2024-03-15 17:08:12 +0100

    up

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
 import { defineConfig } from "vite"
+import { resolve } from "path"
 
-export default defineConfig({})
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+  },
+})

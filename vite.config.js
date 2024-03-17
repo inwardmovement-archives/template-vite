71d5e0eda0e4128ff8755bfb100abe23ff303c8e
Victor Massé <victor.dhamma@gmail.com>
2024-03-17 08:22:43 +0100

    up

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
+import path from "path"
+import { globSync } from "glob"
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
   build: {
-    outDir: "../dist",
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync("src/**/*.html").map(file => [
+          path.relative(
+            "src",
+            file.slice(0, file.length - path.extname(file).length),
+          ),
+          resolve(__dirname, file),
+        ]),
+      ),
+    },
   },
 })
